b9a7b7a8677514db187dde97085ea6fdd2a68cc9
Grzegorz <g.swajda@gmail.com>
2022-07-14 13:07:45 +0200

    Some crap corrected

--- Screens/SelectionTopicScreen.js
+++ Screens/SelectionTopicScreen.js
@@ -1,28 +1,61 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View, SafeAreaView, Button, Platform } from 'react-native';
-import { getAuth, signOut} from 'firebase/auth';
+import { useState } from 'react';
+import { StyleSheet, Text, View, SafeAreaView, FlatList, TouchableOpacity} from 'react-native';
+import { getAuth} from 'firebase/auth';
+
+const DATA = [
+	{
+	  id: 'bd7acbea-c1b1-46c2-aed5-3ad53abb28ba',
+	  title: 'First Item',
+	},
+	{
+	  id: '3ac68afc-c605-48d3-a4f8-fbd91aa97f63',
+	  title: 'Second Item',
+	},
+	{
+	  id: '58694a0f-3da1-471f-bd96-145571e29d72',
+	  title: 'Third Item',
+	},
+  ];
+
+const Item = ({ item, onPress }) => (
+	<TouchableOpacity onPress={onPress}>
+		<Text>{item.title}</Text>
+	</TouchableOpacity>
+  );
 
 export default function HomeScreen({navigation, route}) {
-    
-    const auth = getAuth();
-
-        return (
-            <SafeAreaView style={styles.container}>
-                <View>
-                    <Text>asd</Text>
-                    <Button title="show params" onPress={()=>{console.log(auth)}}/>
-                </View>
-            </SafeAreaView>
-        );
+	
+	const auth = getAuth();
+	const [selectedId, setSelectedId] = useState(null);
+	const renderItem = ({ item }) => {
+		return (
+		  <Item
+			item={item}
+			onPress={() => setSelectedId(item.id)}
+		  />
+		);
+	};
+		return (
+			<SafeAreaView style={styles.container}>
+				<View>
+				<FlatList
+					data={DATA}
+					renderItem={renderItem}
+					keyExtractor={(item) => item.id}
+					extraData={selectedId}
+				/>
+				</View>
+			</SafeAreaView>
+		);
 }
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-    backgroundColor:'dodgerblue',
-    
+	flex: 1,
+	backgroundColor: '#fff',
+	alignItems: 'center',
+	justifyContent: 'center',
+	backgroundColor:'dodgerblue',
+	
   },
 });
