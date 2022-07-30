378f3eab6f7ae9d80b1d36df380a58c1cf7a3f23
Grzegorz <g.swajda@gmail.com>
2022-07-30 22:21:14 +0200

    animated progressBar

--- Screens/TopicScreen.js
+++ Screens/TopicScreen.js
@@ -3,10 +3,25 @@ import { StyleSheet, Text, View, SafeAreaView, Button, StatusBar, Dimensions} fr
 import { LessonBox } from '../components/LessonBox';
 import { TopicTopBar } from '../components/TopicTopBar';
 import { ProgressBar } from '../components/ProgressBar';
+
 const WIDTH = Dimensions.get("screen").width;
 
 export default function TopicScreen({navigation, route}) {
+	var calc = 100/route.params.data.rules.length;
 	const [changer, setChanger] = useState(0)
+	const [progress, setProgress] = useState(100/route.params.data.rules.length);
+	console.log(calc);
+	console.log(calc)
+	function progressEffect(){
+		if(changer == route.params.data.rules.length - 1)
+			{ 
+				setChanger(0);
+				setProgress(100/route.params.data.rules.length);	 
+			}else {
+				setChanger(changer + 1)
+				setProgress(progress + (100/route.params.data.rules.length))
+			}			
+	}
 	return (
 	    <SafeAreaView style={styles.container}>
 			<TopicTopBar title={route.params.data.key}/>
@@ -17,7 +32,7 @@ export default function TopicScreen({navigation, route}) {
 					radiusBottom={20}
 					secondColor={'#FEECE9'}
 					firstColor={'#2F3A8F'}
-					progress={route.params.data.progress}
+					progress={progress}
 				/>
 			</View>
             <View style={{marginTop:35}}>
@@ -26,7 +41,7 @@ export default function TopicScreen({navigation, route}) {
 					example={route.params.data.example}
 					i={changer}
 				/>
-				<Button title='klik' onPress={()=>{if(changer == route.params.data.rules.length - 1){ setChanger(0) }else {setChanger(changer + 1)}}}/>
+				<Button title='klik' onPress={progressEffect}/>
             </View>
 		</SafeAreaView>
 	);
--- components/ProgressBar.js
+++ components/ProgressBar.js
@@ -1,4 +1,5 @@
-import {Text, View} from 'react-native';
+import {Text, View, Animated} from 'react-native';
+import { useRef, useState, useEffect } from 'react';
 
 function check(show, progress){
     if(show === true){
@@ -10,27 +11,37 @@ function check(show, progress){
     }
 }
 
-export const ProgressBar = ({width = 100, height, firstColor, secondColor, progress, radiusBottom, radius, showValue})=> (
-	<View style={{flexDirection:'row',alignItems:'center'}}>
-        <View style={{
-            width:width,
-            height:height,
-            backgroundColor:secondColor,
-            borderRadius:radius,
-            borderBottomLeftRadius: radiusBottom,
-            borderBottomRightRadius: radiusBottom
-        }}>
+export function ProgressBar({width = 100, height, firstColor, secondColor, progress, radiusBottom, radius, showValue}){
+    const [prog, setProg] = useState(0);
+
+    useEffect(()=>{
+        for (let i = prog; i <= progress; i++) {
+            setTimeout(() => {
+                setProg(i);
+            }, 5 * i);
+        }
+    },[progress])
+
+    return(
+        <View style={{flexDirection:'row',alignItems:'center'}}>
             <View style={{
-                width: progress !== undefined ? String(progress)+'%' : 100,
+                width:width,
                 height:height,
-                backgroundColor:firstColor,
+                backgroundColor:secondColor,
                 borderRadius:radius,
                 borderBottomLeftRadius: radiusBottom,
                 borderBottomRightRadius: radiusBottom
             }}>
-
+                <View style={{
+                    width: progress !== undefined ? String(prog)+'%' : 100,
+                    height:height,
+                    backgroundColor:firstColor,
+                    borderRadius:radius,
+                    borderBottomLeftRadius: radiusBottom,
+                    borderBottomRightRadius: radiusBottom
+                }}>
+                </View>
             </View>
+                {check(showValue, progress)}
         </View>
-            {check(showValue, progress)}
-    </View>
-);
+)};
